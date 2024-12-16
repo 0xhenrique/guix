c9197e37d2e8e0e30f0391c0cce1eb5ea8dce91d
0xhenrique <allyson2308h@gmail.com>
2024-12-16 04:13:08 +0000

    update guix config files

--- guix/config.scm
+++ guix/config.scm
@@ -9,18 +9,16 @@
 
 ;; Indicate which modules to import to access the variables
 ;; used in this configuration.
-(use-modules (nongnu packages linux)
-	     (nongnu system linux-initrd)
-	     (gnu)
-	     (gnu services)
-	     (gnu services desktop)
-	     (gnu packages))
+(use-modules
+ (gnu)
+ (gnu services samba)
+ (gnu services nginx)
+ (nongnu packages linux))
 (use-service-modules cups desktop networking ssh xorg)
 
 (operating-system
-  (kernel linux)
-  (initrd microcode-initrd)
-  (firmware (list linux-firmware))
+ (kernel linux)
+ (firmware (list linux-firmware))
   (locale "en_GB.utf8")
   (timezone "Europe/Lisbon")
   (keyboard-layout (keyboard-layout "us"))
@@ -28,10 +26,10 @@
 
   ;; The list of user accounts ('root' is implicit).
   (users (cons* (user-account
-                  (name "void")
-                  (comment "void")
+                  (name "arisu")
+                  (comment "arisu")
                   (group "users")
-                  (home-directory "/home/void")
+                  (home-directory "/home/arisu")
                   (supplementary-groups '("wheel" "netdev" "audio" "video")))
                 %base-user-accounts))
 
@@ -40,43 +38,60 @@
   ;; for packages and 'guix install PACKAGE' to install a package.
   (packages (append (list (specification->package "emacs")
                           (specification->package "emacs-exwm")
-                          (specification->package "i3-wm")
                           (specification->package
-                           "emacs-desktop-environment")
-                          (specification->package "nss-certs"))
-                    %base-packages))
+                           "emacs-desktop-environment")) %base-packages))
 
   ;; Below is the list of system services.  To search for available
   ;; services, run 'guix system search KEYWORD' in a terminal.
-  (services
-   (append (list (service xfce-desktop-service-type)
-                 (service cups-service-type)
-		 (service bluetooth-service-type
-			  (bluetooth-configuration
-			   (auto-enable? #t)))
-                 (set-xorg-configuration
-                  (xorg-configuration (keyboard-layout keyboard-layout))))
-           %desktop-services))
-  
+(services
+ (append (list
+          (service gnome-desktop-service-type)
+          (service samba-service-type
+                   (samba-configuration
+                    (enable-smbd? #t)
+                    (config-file (plain-file "smb.conf" "\
+                                              [global]
+                                              map to guest = Bad User
+                                              logging = syslog@1
+                                              
+                                              [public]
+                                              browsable = yes
+                                              path = /public
+                                              read only = no
+                                              guest ok = yes
+                                              guest only = yes\n"))))
+          (set-xorg-configuration
+           (xorg-configuration (keyboard-layout keyboard-layout))))
+         (modify-services %desktop-services
+           (guix-service-type config =>
+             (guix-configuration
+              (inherit config)
+              (substitute-urls
+               (append (list "https://substitutes.nonguix.org")
+                       %default-substitute-urls))
+              (authorized-keys
+               (append (list (local-file "./signing-key.pub"))
+                       %default-authorized-guix-keys)))))))
+
   (bootloader (bootloader-configuration
-                (bootloader grub-efi-bootloader)
-                (targets (list "/boot/efi"))
+                (bootloader grub-bootloader)
+                (targets (list "/dev/nvme0n1"))
                 (keyboard-layout keyboard-layout)))
   (swap-devices (list (swap-space
                         (target (uuid
-                                 "9be4a357-291f-4f11-a3ae-114916485157")))))
+                                 "bd49f319-3190-4144-ac8d-fdceada559c8")))))
 
   ;; The list of file systems that get "mounted".  The unique
   ;; file system identifiers there ("UUIDs") can be obtained
   ;; by running 'blkid' in a terminal.
   (file-systems (cons* (file-system
                          (mount-point "/boot/efi")
-                         (device (uuid "BBF6-2761"
+                         (device (uuid "73D8-0D0A"
                                        'fat32))
                          (type "vfat"))
                        (file-system
                          (mount-point "/")
                          (device (uuid
-                                  "c1ef2bac-ba38-4d0d-9bb4-0cb85e9edec5"
+                                  "1326068e-af88-4723-80d1-c69029201c8d"
                                   'ext4))
                          (type "ext4")) %base-file-systems)))
